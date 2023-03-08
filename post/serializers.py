2c86afd1ae05cf678c0f41eaefee8b11832cba5c
madyanes <madyanes.1995@gmail.com>
2023-03-08 14:14:27 +0700

    change Post serializer type

--- post/serializers.py
+++ post/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from post import models
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Post
         fields = '__all__'
