3851900e5788a49c88650c72148cf7b56e8ea5da
madyanes <madyanes.1995@gmail.com>
2023-03-14 00:00:09 +0700

    command to create superuser

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ pip install -r requirements.txt
 echo "Collect static..."
 python3.9 manage.py collectstatic
 
+echo "Create supseruser..."
+python3.9 manage.py createsuperuser --username superuser --password superuser
+
 echo "Make migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
